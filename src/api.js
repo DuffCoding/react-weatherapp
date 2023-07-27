722cd89c0ec1498f0db3180a211c7781a41e244a
Jon Duff <duffmanvb@yahoo.com>
2023-07-27 17:54:34 -0500

    search bar work name country latitude longitude

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
-const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
-const options = {
+export const geoApiOptions = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'f56767ca22msh095804e25d7aad0p1b03f8jsnd533eb49c7d6',
@@ -7,10 +6,4 @@ const options = {
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+import 
 
 const Search = ({onSearchChange}) => {
 
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
-        
+       return fetch('${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}',
+       geoApiOptions
+    )
+        .then(response => response.json())
+        .then(response => {
+            return {
+                options: response.data.map((city) => {
+                    return {
+                        value: '${city.latitude} ${cit.longitude}' ,
+                        label: '${city.name} ${city.country}',
+                    }
+                })
+            }
+        })
+        .catch((err) => console.error(err));
     }
 
     const handleOnChange = (searchData) => {
